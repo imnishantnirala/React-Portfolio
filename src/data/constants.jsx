108a1ec5aef3cb6d7c8da8ad1ddcad3d99fa0042
Nishant Nirala <niralanishant331@gmail.com>
2024-07-03 20:34:26 +0530

    resume link update

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -15,7 +15,7 @@ export const aboutData = {
     title: 'Hi, I am Nishant Nirala',
     profile: 'I am a Software Developer.',
     buttonText: 'Check Resume',
-    resumeLink: "https://drive.google.com/file/d/1_F-j7zoyqF5WpMYm8d-JsEgGXQ-Uo_dJ/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1SVW4LaNV0zItLq5flv0_yuNqhg5XDIxM/view?usp=sharing",
     buttonClasses: 'text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2',
     profileImage: '../assets/1673502997607.jpeg',
     imageClasses: 'rounded-full shadow-2xl dark:shadow-gray-900 ring-4 ring-gray-300'
