accda4b23c20448e479521799259c201ef586325
Nishant Nirala <niralanishant331@gmail.com>
2024-07-05 00:49:03 +0530

    some improvment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,13 @@ function About() {
 
   return (
     <div className='relative container mx-auto px-4 py-10' id='About'>
-      <div className='absolute inset-0 md:hidden opacity-10'>
-        <img src={profileImg} className={imageClasses +  'object-cover mt-20'} alt='Background' />
+      <div className='absolute inset-0 md:hidden opacity-20'>
+        <img src={profileImg} className={imageClasses + ' border-white object-cover mt-20'} alt='Background' />
       </div>
       <div className='relative flex flex-col md:flex-row justify-center p-5 bg-opacity-90 md:bg-opacity-100'>
-        <div className='md:w-[700px] p-6 text-[20px] md:text-[25px] mt-10 md:mt-20'>
-          <h1 className='text-[30px] md:text-[40px] font-bold'>{title}</h1>
-          <h1>{profile}</h1>
+        <div className='md:w-[700px] p-0 md:p-6 text-[17px] md:text-[25px] mt-10 md:mt-20'>
+          <h1 className='text-[20px] md:text-[40px] font-bold'>{title}</h1>
+          <h1 className='text-[15px] md:text-[20px]'>{profile}</h1>
           <h3 className='pt-5'>{aboutText}</h3>
           <div className='mt-5'>
             <a href={resumeLink} target='_blank' rel='noopener noreferrer'>
--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -81,7 +81,7 @@ export const skillsData = [
       skills: [
         {
           name: 'Git',
-          image: 'https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667',
+          image: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
         },
         {
           name: 'GitHub',
@@ -165,7 +165,7 @@ export const educationData = [
     id: 2,
     institution: 'Bihar Board Intermediate',
     degree: '12th Math',
-    period: 'Dec 2019 - Dec 2021',
+    period: 'Dec 2016 - Dec 2018',
     logo: 'https://images.collegedunia.com/public/college_data/images/logos/BSEB_LOGO.png',
     description: 'I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.',
   },
